5098685c2c937c9e9238117bc993cd1e11f8484e
RaisaNuzhat <raisa.csecu@gmail.com>
2024-06-22 16:26:02 +0600

    role change done

--- index.js
+++ index.js
@@ -95,6 +95,19 @@ async function run() {
                   res.send(result)
               })
 
+                  //update a user role
+              app.patch('/users/update/:email', async (req, res) => {
+                const email = req.params.email
+                const user = req.body
+                const query = { email }
+                const updateDoc = {
+                  $set: { ...user, timestamp: Date.now() },
+                }
+                const result = await userCollection.updateOne(query, updateDoc)
+                res.send(result)
+              })
+
+
          //get by id for view details
          app.get('/pets/:id',async(req,res) =>
           {
